6bf77a50cac55ec137bc434cbb7082fbc8f2c686
marudits <marudi@coss.io>
2019-03-31 23:07:41 +0800

    :sparkles: NEW FEATURES | Process order mechainsm

--- frontend/src/components/WineList.vue
+++ frontend/src/components/WineList.vue
@@ -1,14 +1,17 @@
 <template lang="pug">
     .wine-list
-        el-input(placeholder="Filter wines" v-model="filter")
         el-row(:gutter="8")
             el-col(:span="14")
+                el-input(placeholder="Filter wines" v-model="filter")
                 el-tree.filter-tree(
+                    v-if="wines.list && wines.list.length > 0"
                     :data="wines.list" v-model="wines.selected" :props="wines.props" 
                     ref="wineList" show-checkbox
                     @check-change="handleChange"
                     :filter-node-method="filterList"
+                    node-key="id"
                     )
+                h4 There is no available wine(s) at the moment.
             el-col(:span="10")
                 el-card.box-card.order
                     div(slot="header").clearfix.order-title
@@ -28,9 +31,7 @@
                                 h4 &nbsp;$ {{ order.total }}
                         el-row
                             el-col(align="right")
-                                el-button(type="success") Checkout
-                            
-
+                                el-button(type="success" @click="processOrder") Checkout
         
 </template>
 
@@ -58,7 +59,8 @@ export default {
                 selected: [],
                 props: {
                     children: '',
-                    label: 'title'
+                    label: 'label',
+                    disabled: 'disabled'
                 },
             },
         }
@@ -70,8 +72,7 @@ export default {
             }
             return data.label.indexOf(value.toUpperCase()) !== -1;
         },
-        handleChange(data, checked, indeterminate){
-            console.log(data, checked, indeterminate);
+        handleChange(data, checked){
             if(checked){
                 this.wines.selected.push(data);
                 this.order.total += parseInt(data.price.replace('$', ''));
@@ -86,20 +87,48 @@ export default {
                     if(res.body.successful){
                         this.wines.list = res.body.payload.map(x => 
                             Object.assign({}, x, {
-                                key: x.guid.split('/')[x.guid.split('/').length - 1],
-                                label: x.title,
-                                title: `${getWineName(x.title)} - ${getWinePrice(x.title)}`,
+                                id: x.guid.split('/')[x.guid.split('/').length - 1],
+                                label: `${getWineName(x.title)} - ${getWinePrice(x.title)}`,
                                 price: getWinePrice(x.title),
-                                name: getWineName(x.title)
+                                name: getWineName(x.title),
+                                disabled: !x.isAvailableToday
                             })
                         )
                         .sort((a,b) => a.title.localeCompare(b.title));
                     }
                 })
-                .catch(err => {
-                    console.error('Failed on load winee list: ', err);
+                .catch(() => {
+                     this.showMessage('Failed on get wine list.', 'error');
                 })
-        }
+        },
+        processOrder(){
+            let payload = this.wines.selected.map(x => x.id).join(',')
+            
+            wineService.orderWines(payload)
+                .then(res => {
+                    if(res.body.successful){
+                        let unavailableOrders = res.body.payload.filter(x => !x.isAvailableToday);
+
+                        if(unavailableOrders.length === 0){
+                            this.showMessage(`Success on processing your orders. Please wait for your menu to be served. Thank you.`, 'success');
+                            this.wines.selected = [];
+                            this.$refs.wineList.setCheckedNodes(this.wines.selected);
+                        } else {
+                            let unavailableWines = unavailableOrders.map(x => getWineName(x.title)).join(', ');
+                            this.showMessage(`Unfortunately ${unavailableWines} not available today. Please choose another wine(s).`, 'warning');
+                        }
+
+                    } else {
+                        this.showMessage('Failed on process order. Please try again after a moment.', 'error');
+                    }
+                })
+                .catch(() => {
+                    this.showMessage('Failed on process order. Please try again after a moment.', 'error');
+                })
+        },
+        showMessage(message = 'Lorem ipsum dolor sit amet', type = 'message',){
+            this.$message({ type, message })
+        },
     },
     watch: {
         filter(val) {
@@ -113,7 +142,6 @@ export default {
 <style lang="scss">
     .wine-list {
         .order {
-            margin-top: .5rem;
             &-title {
                 margin: 0;
 
